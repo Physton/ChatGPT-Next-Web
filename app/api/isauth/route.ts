eaf203a679a9ff4e626246633ce6b2836c4dcb3b
physton <physton@163.com>
2024-01-04 16:55:00 +0800

    Update route.ts

--- app/api/isauth/route.ts
+++ app/api/isauth/route.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import { GEMINI_BASE_URL, Google, ModelProvider } from "@/app/constant";
 
 import { auth } from "../auth";
 
 async function handle(req: NextRequest) {
-  const res = { isAuth: !auth(req).error };
+  const res = { isAuth: !auth(req, ModelProvider.GeminiPro).error };
   return NextResponse.json(res);
 }
 
